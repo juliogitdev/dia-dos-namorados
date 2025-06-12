394664033fe9036ba5f94274354ed97f41d84a48
juliogitdev <juliovieira035@gmail.com>
2025-06-12 16:06:48 -0300

    Adicionadno novo metodo de musica

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Doom carregado");
+  const audio = document.getElementById('musica');
+  audio.play().then(() => {
+    console.log("Musica começando...")
+  });
+})
+
 function entrarSite() {
   document.querySelector('.entrada').style.display = 'none';
   document.querySelector('.conteudo').style.display = 'block';
-  document.getElementById('musica').play();
 }
