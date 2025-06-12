7acc3755de7bb7398c59b8d4d5867dbdf7ca205c
juliogitdev <juliovieira035@gmail.com>
2025-06-12 14:52:50 -0300

    Final

---
+++ script.js
@@ -0,0 +1,5 @@
+function entrarSite() {
+  document.querySelector('.entrada').style.display = 'none';
+  document.querySelector('.conteudo').style.display = 'block';
+  document.getElementById('musica').play();
+}
